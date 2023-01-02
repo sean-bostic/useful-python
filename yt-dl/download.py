b5601b09ae6f185d9dd2ad7cb38046b123396a03
Sean Bostic <sean.bostic@gmail.com>
2023-01-02 15:32:35 -0500

    Update download.py

--- yt-dl/download.py
+++ yt-dl/download.py
@@ -1,14 +1,19 @@
+from urllib.error import HTTPError
 from pytube import YouTube
 from typer import Typer
+import shutil
 
 app = Typer()
 
 @app.command()
-def download_vid(url: str, output: str = "./"):
+def download_vid(url: str, output: str = "./vids"):
     video = YouTube(url)
     stream = video.streams.get_highest_resolution()
-    stream.download()
 
+    try:
+        stream.download()
+    except HTTPError as e:
+        print(f"An error occurred: {e} ")
 
 if __name__ == "__main__":
     app()
